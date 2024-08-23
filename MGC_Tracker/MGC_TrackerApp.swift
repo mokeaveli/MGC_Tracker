128ca126bb3d7b54950f7fcfbce4831ca71e07ff
mokeaveli <m.s.stanton@outlook.com>
2024-08-23 15:14:17 +0100

    Initial Commit

---
+++ MGC_Tracker/MGC_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MGC_TrackerApp.swift
+//  MGC_Tracker
+//
+//  Created by Mike on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MGC_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MGC_TrackerUITests/MGC_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MGC_TrackerUITestsLaunchTests.swift
+//  MGC_TrackerUITests
+//
+//  Created by Mike on 23/08/2024.
+//
+
+import XCTest
+
+final class MGC_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
